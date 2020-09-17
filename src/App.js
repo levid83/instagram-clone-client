98cc27c450674715bc6b3877064c9bd0ba9464ef
Levente Danel <danellevente@yahoo.com>
2020-09-17 18:23:20 +0300

    create user context and wire it with global state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useReducer } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { reducer, initialState } from "./reducers/userReducer";
+
+import "./App.css";
+
+export const UserContext = createContext();
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter></BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,5 @@
+export const initialState = null;
+
+export const reducer = (state, action) => {
+  return state;
+};
