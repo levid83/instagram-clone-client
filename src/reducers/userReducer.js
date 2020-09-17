3509f9cde9e3e07257572ad0dd6884a258a64999
Levente Danel <danellevente@yahoo.com>
2020-09-17 19:11:55 +0300

    complete user state reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,5 +1,30 @@
 export const initialState = null;
 
+export const Actions = {
+  SET_USER: "SET_USER",
+  LOGOUT: "LOGOUT",
+  UPDATE_FOLLOWERS: "UPDATE_FOLLOWERS",
+  UPDATE_PICTURE: "UPDATE_PICTURE",
+};
 export const reducer = (state, action) => {
+  if (action.type === "USER") {
+    return action.payload;
+  }
+  if (action.type === "LOGOUT") {
+    return null;
+  }
+  if (action.type === "UPDATE") {
+    return {
+      ...state,
+      followers: action.payload.followers,
+      following: action.payload.following,
+    };
+  }
+  if (action.type === "UPDATE_PICTURE") {
+    return {
+      ...state,
+      picture: action.payload,
+    };
+  }
   return state;
 };
