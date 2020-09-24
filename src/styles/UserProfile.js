681ffb56ecf59bc32d7e2f2dd53bde83a169829d
Levente Danel <danellevente@yahoo.com>
2020-09-24 21:40:08 +0300

    create user profile style comp

---
+++ src/styles/UserProfile.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { device } from "./devices";
+import { Card } from "./Card";
+
+export const UserProfile = styled(Card)`
+  @media ${device.small} {
+    max-width: 70%;
+  }
+  @media ${device.medium} {
+    max-width: 60%;
+  }
+
+  @media ${device.large} {
+    max-width: 40%;
+  }
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  & .picture {
+    align-self: center;
+  }
+`;
